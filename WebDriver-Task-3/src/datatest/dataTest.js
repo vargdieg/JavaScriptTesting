2bbc0bc7016def4f1b7c102f36044b9838fb36d6
dfvargas <dfvargas@bancolombia.com.co>
2024-06-02 14:57:10 -0500

    Se actualiza la sincronia al momento de realizar validaciones

--- WebDriver-Task-3/src/datatest/dataTest.js
+++ WebDriver-Task-3/src/datatest/dataTest.js
@@ -1,4 +1,4 @@
-const createInstance = [
+const createInstance =
     {
     name: 'MyFirstInstance',
     numberOfInstances : 4,
@@ -13,20 +13,6 @@ const createInstance = [
     localSSDValue : '3x375',
     regionValue : 'uswest1',
     commitedSelection : 'year',
-},
-{
-    name: 'MySecondInstance',
-    numberOfInstances : 2,
-    operatingSystemValue : 'paidubuntu',
-    provisionModelSelection : 'spot',
-    machineFamilyValue : 'generalpurpose',
-    seriesMachineValue : 'n2',
-    machineTypeValue : 'n2standar2',
-    addGPU : 'false',
-    localSSDValue : '3x375',
-    regionValue : 'uscentral1',
-    commitedSelection : 'year3',
-},
-]
+}
 
 module.exports = createInstance;
